9ba9eacc967455d4dd276454cb63735fb39c6782
pauloENP <paulocc20@hotmail.com>
2023-01-01 00:48:35 -0300

    updating display

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-class Calculator{
-    constructor(previousOperandTextElement, currentOperandTextElement){
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
-    }
+
+class Calculator {
+        constructor(previousOperandTextElement, currentOperandTextElement){
+            this.previousOperandTextElement = previousOperandTextElement;
+            this.currentOperandTextElement = currentOperandTextElement;
+            this.clear();
+        }
+        clear(){
+        this.currentOperand = '';
+        this.previusOperand = '';
+        this.operation = undefined;
+        }
+        //const calculator = new calculator(previusOperandTextElement, currentOperandTextElement)
+
+        delete(){
+
+        }
+
+        appendNumber(number){
+            this.currentOperand = number
+        
+            /* numberButtons.forEach(button => {
+                button.addEventListener('click', () => {
+                    calculator.appendNumber(button.innerText)
+                    calculator.updateDisplay()
+                })
+            })*/
+        }
+
+        chooseOperation(operation) {
+
+        }
+
+        compute() {
+
+        }
+
+        updateDisplay() {
+            console.log("updating display")
+            this.currentOperandTextElement.innerText = this.currentOperand
+        }
 }
 
-
-
-
-
-clear() {
-    this.currentOperand = ''
-    this.previusOperand = ''
-    this.operation = undefined
-
-    //const calculator = new calculator(previusOperandTextElement, currentOperandTextElement)
-
-}
-delete(){
-
-}
-appendNumber(number){
-    this.currentOperand = number
-   
-    /* numberButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            calculator.appendNumber(button.innerText)
-            calculator.updateDisplay()
-        })
-    })*/
-}
-
-chooseOperation(operation) {
-
-}
-compute() {
-
-}
-updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand
-}
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const deleteButton = document.querySelector('[data-delete]')
@@ -51,8 +51,9 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-numberButtons.forEach(button =>{
+numberButtons.forEach(button => {
     button.addEventListener('click', () =>{
+        console.log("test")
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
